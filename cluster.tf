723791cc91b101d4fa8ded6a3b9cafac2a54e527
Muhammed Kadir KARAKOC <m.kadirkarakoc@gmail.com>
2020-12-06 23:16:31 +0300

    cluster.tf updated

--- cluster.tf
+++ cluster.tf
@@ -10,3 +10,15 @@ module "network" {
   subnetwork_pods     = "10.41.0.0/16"
   subnetwork_services = "10.42.0.0/16"
 }
+
+module "cluster" {
+  source                           = "git@github.com:FairwindsOps/terraform-gke.git//vpc-native?ref=vpc-native-v1.2.0"
+  region                           = "us-central1"
+  name                             = "gke-example"
+  project                          = "terraform-module-cluster"
+  network_name                     = "kube"
+  nodes_subnetwork_name            = module.network.subnetwork
+  kubernetes_version               = "1.16.10-gke.8"
+  pods_secondary_ip_range_name     = module.network.gke_pods_1
+  services_secondary_ip_range_name = module.network.gke_services_1
+}
