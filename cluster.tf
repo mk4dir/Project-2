585262338c6034b6ce78b0374ddc8d733d6c7271
Muhammed Kadir KARAKOC <m.kadirkarakoc@gmail.com>
2020-12-06 23:15:35 +0300

    cluster.tf added

---
+++ cluster.tf
@@ -0,0 +1,12 @@
+module "network" {
+  source = "git@github.com:FairwindsOps/terraform-gcp-vpc-native.git//default?ref=default-v2.1.0"
+  // base network parameters
+  network_name     = "kube"
+  subnetwork_name  = "kube-subnet"
+  region           = "us-central1"
+  enable_flow_logs = "false"
+  // subnetwork primary and secondary CIDRS for IP aliasing
+  subnetwork_range    = "10.40.0.0/16"
+  subnetwork_pods     = "10.41.0.0/16"
+  subnetwork_services = "10.42.0.0/16"
+}
